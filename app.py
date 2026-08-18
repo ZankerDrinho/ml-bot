d716706ff924a6cc3251f549bdc4ffa1b6541826
ZankerDrinho <pedro.ronaldo2786@gmail.com>
2026-08-17 22:56:01 -0300

    Adiciona callback inicial do OAuth

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # API de teste
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from produtos import products
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "Olá! Meu bot está funcionando"
+    return "Funcionou :D"
 
 
 @app.route("/produtos")
 def produto():
     return jsonify(products)
+
+@app.route("/oauth/callback")
+def oauth_callback():
+    code = request.args.get("code")
+    state = request.args.gt("state")
+
+    return {
+        "mensagem": "Callback recebido!",
+        "code": code,
+        "state": state
+    }
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
